968fd3f359e61da3d3c26dcd601c3ba8ea16dd70
Joshua141822 <152835426+Joshua141822@users.noreply.github.com>
2024-01-20 03:31:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showAlert(buttonName) {
+  alert(`You clicked the ${buttonName} button!`);
+}
+
+function navigate(url) {
+  window.location.href = url;
+}
+
+function navigate(page) {
+  // Use window.location.href to navigate to the specified page
+  window.location.href = page;
+}
